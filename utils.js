e2a7a04dd60455da1a1a5b650fefeb50f2148a1d
Ndivine-eng <dnubuhoro@alustudent.com>
2025-03-19 02:00:41 +0300

    new commit

--- utils.js
+++
@@ -1,9 +0,0 @@
-// **Sanitize User Input**
-function sanitizeInput(input) {
-    return encodeURIComponent(input);
-}
-
-// **Generate CSRF Token**
-function generateCSRFToken() {
-    return Math.random().toString(36).substr(2);
-}
